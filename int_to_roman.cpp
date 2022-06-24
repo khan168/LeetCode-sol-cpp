166d8777e67f0c3cb1fd43c033520973b9cfaecb
khan168 <87687164+khan168@users.noreply.github.com>
2022-06-24 16:53:50 +0530

    Properly formatted for test-cases

--- int_to_roman.cpp
+++ int_to_roman.cpp
@@ -1,32 +1,38 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-// bool compare(string s1, string s2)
-// {
-//     return s1.size() < s2.size();
-// }
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        map<int, string> mapa = {{1, "I"}, {5, "V"}, {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}, {4, "IV"}, {9, "IX"}, {40, "XL"}, {90, "XC"}, {400, "CD"}, {900, "CM"}};
+        string ans = "";
+        while (num != 0)
+        {
+            auto c = mapa.rbegin();
+            while (c != mapa.rend())
+            {
+                if (c->first <= num)
+                {
+                    num -= c->first;
+                    ans += c->second;
+                    break;
+                }
+                c++;
+            }
+        }
+
+        return ans;
+    }
+};
 
 int main()
 {
-    int num = 32;
-    map<int, string> mapa = {{1, "I"}, {5, "V"}, {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}, {4, "IV"}, {9, "IX"}, {40, "XL"}, {90, "XC"}, {400, "CD"}, {900, "CM"}};
-    string ans = "";
-    while (num != 0)
-    {
-        auto c = mapa.rbegin();
-        while (c != mapa.rend())
-        {
-            if (c->first <= num)
-            {
-                num -= c->first;
-                ans += c->second;
-                break;
-            }
-            c++;
-        }
-    }
-
-    cout << ans;
+    Solution mysol = Solution();
+    // testcase1:3 Expected Output III
+    cout << mysol.intToRoman(3);
+    // testcase1:58 Expected Output LVIII
+    // testcase1:1994 Expected Output MCMXCIV
 }
