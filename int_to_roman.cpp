bc9ed588d27335753e271078b2f1ae9c2feeb613
khan168 <87687164+khan168@users.noreply.github.com>
2022-06-25 10:29:46 +0530

    more efficient solution
    more efficient in terms of run time.

--- int_to_roman.cpp
+++ int_to_roman.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 typedef long long ll;
 
-class Solution
-{
+class Solution {
 public:
-    string intToRoman(int num)
-    {
-        map<int, string> mapa = {{1, "I"}, {5, "V"}, {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}, {4, "IV"}, {9, "IX"}, {40, "XL"}, {90, "XC"}, {400, "CD"}, {900, "CM"}};
-        string ans = "";
-        while (num != 0)
-        {
-            auto c = mapa.rbegin();
-            while (c != mapa.rend())
-            {
-                if (c->first <= num)
-                {
-                    num -= c->first;
-                    ans += c->second;
-                    break;
-                }
-                c++;
+    string intToRoman(int num) {
+        map<int,string> mapa={{1,"I"},{5,"V"},{10,"X"},{50,"L"},{100,"C"},{500,"D"},{1000,"M"},{4,"IV"},{9,"IX"},{40,"XL"},{90,"XC"},{400,"CD"},{900,"CM"}};
+        string ans="";
+        auto it = mapa.rbegin();
+        while(it!=mapa.rend()){
+            int t= num/(it->first);
+            if(t>0){
+               while(t--){
+                ans+=it->second;
+                
+                } 
+                num=num%it->first;
             }
+            
+            
+            it++;
         }
-
+        
         return ans;
+        
+        
     }
 };
 
