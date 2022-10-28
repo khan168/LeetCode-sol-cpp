721fa0b487c85f1d067f0c4b253144a64c7668f7
khan162 <umarahmedkhank6@gmail.com>
2022-10-28 15:05:11 -0400

    made a new folder for array questions

---
+++ Neetcode150/Array&Hashing/containsdup.cpp
@@ -0,0 +1,41 @@
+//1 using sets
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.find(nums[i]) == s.end())
+            {
+                s.insert(nums[i]);
+            }
+            else
+            {
+                if (s.count(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    s.insert(nums[i]);
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//using sets but smaller implementation
+//2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return nums.size() > set<int>(nums.begin(),nums.end()).size();
+    }
+};
