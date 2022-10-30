a463d8db55aab2a642201dff114f07f911d8e402
khan162 <umarahmedkhank6@gmail.com>
2022-10-30 13:37:50 -0400

    first solution

---
+++ Neetcode150/Array&Hashing/top-k-frequent-elements.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+//function to sort the pairs according to second value in descending order
+struct less_than_key
+{
+    inline bool operator()(const pair<int, int> &p1, const pair<int, int> &p2)
+    {
+        return (p1.second > p2.second);
+    }
+};
+
+/*
+Through counting using map make pair of vector and sort pair according to second value in descending order
+*/
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        vector<pair<int, int>> v;
+        vector<int> ans;
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            m[nums[i]]++;
+        }
+        for (auto i : m)
+        {
+            v.push_back(make_pair(i.first, i.second));
+        }
+        sort(v.begin(), v.end(), less_than_key());
+        for (auto i : v)
+        {
+            if (ans.size() < k)
+                ans.push_back(i.first);
+        }
+        return ans;
+    }
+};
