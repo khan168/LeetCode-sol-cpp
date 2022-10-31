31c989add5844f46bb86d7030aa7aa0782af7ef0
khan168 <87687164+khan168@users.noreply.github.com>
2022-10-31 08:28:12 +0530

    using maxheap

--- Neetcode150/Array&Hashing/top-k-frequent-elements.cpp
+++ Neetcode150/Array&Hashing/top-k-frequent-elements.cpp
@@ -40,3 +40,24 @@ public:
         return ans;
     }
 };
+
+//approach 2:
+//using max heap 
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<pair<int,int>> v;
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        for(auto i:m){ pq.push({i.second,i.first});}
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
